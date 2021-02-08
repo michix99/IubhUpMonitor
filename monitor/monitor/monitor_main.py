2357f03e3d18bad6450d6947658a1e7d6f1e480e
Daniel Czeppel <danielczeppel@gmail.com>
2021-02-08 19:58:45 +0100

    autoloading existing data in monitor

--- monitor/monitor/monitor_main.py
+++ monitor/monitor/monitor_main.py
@@ -17,19 +17,23 @@ log_timer = 0
 log_interval = 15
 
 for site in websites:
+    with open(site.name + ".json", "r") as file:
+        site.availability = monitor.read_json(file).availability
     print("Starting monitoring of " + site.name)
+
 while True:
     for site in websites:
         request = requests.get(site.url)
         up = 1 if request.status_code == 200 else 0
         site.availability.append((int(time.time()), up))
-        print("Checking: " + site.name + " Status: " + str(up) + "; ", end="")
+        status = str(up) if up == 1 else request.status_code
+        print("Checking: " + site.name + " Status: " + status + "; ", end="")
         monitor.create_json(site)
         time.sleep(2)
-    print()
     if log_timer % log_interval == 0:
+        print("")
         print("===============================STATUS UPDATE===============================")
-        print("Monitoring for " + str(log_timer*log_interval) + " minutes")
+        print("Monitoring for " + str(log_timer) + " minutes")
         for site in websites:
             avg_up = 0
             for d in site.availability:
