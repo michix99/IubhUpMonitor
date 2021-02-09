4c99cb4ede2a6ab2e1eb964cf98e4a53674d983f
Daniel Czeppel <danielczeppel@gmail.com>
2021-02-09 17:37:16 +0100

    Improved features and stability of the monitor
    -checks internet connection before trying to request pages
    -handles timeouts and requests error appropriately
    -improved structuring and performance
    -added comments

--- monitor/monitor/monitor_main.py
+++ monitor/monitor/monitor_main.py
@@ -2,46 +2,96 @@ import requests
 import monitor
 import time
 
-websites = []
-myCampus = monitor.Website("myCampus", "https://mycampus.iubh.de/")
-webreader = monitor.Website("webreader", "https://iubh.webreader.io/")
-brainyoo = monitor.Website("BrainYoo", "https://brainyoo-webversion.iubh-fernstudium.de/")
-care = monitor.Website("Care", "https://care-fs.iubh.de/")
-websites.append(myCampus)
-websites.append(webreader)
-websites.append(brainyoo)
-websites.append(care)
+# The pages we use to make sure we are online
+sanity_checks = [monitor.Website("Google", "https://www.google.com"),
+                 monitor.Website("Amazon", "https://www.amazon.de/"),
+                 monitor.Website("Facebook", "https://www.facebook.com")]
 
-sleep_time = 60
-log_timer = 0
-log_interval = 15
+# The pages we intend to monitor
+websites = [monitor.Website("myCampus", "https://mycampus.iubh.de/"),
+            monitor.Website("webreader", "https://iubh.webreader.io/"),
+            monitor.Website("BrainYoo", "https://brainyoo-webversion.iubh-fernstudium.de/"),
+            monitor.Website("Care-FS", "https://care-fs.iubh.de/")]
 
+
+# Check pages that are typically available to make sure the problem is not on our side
+def are_we_online():
+    for check_site in sanity_checks:
+        if check_availability(check_site, save=False, log=False) == "200":
+            return True
+    return False
+
+
+# Check if we already have data and if so append it to our new objects
+def load_existing_data(website):
+    try:
+        with open(site.name + ".json", "r") as file:
+            site.availability = monitor.read_json(file).availability
+            print("Loaded existing data for " + site.name)
+    except FileNotFoundError:
+        print("Could not find existing data for " + site.name)
+
+
+# Check if website is available and returns status as string
+#   save: Should the data be saved locally?
+#   log: Should the information be logged in the console? (Will not create linebreaks but tab at end!)
+#
+#   Possible return values:
+#   -HTML Status code (200 = Okay, 503 = Service Unavailable etc.)
+#   -"-999" in case of potential timeouts or other request errors
+
+def check_availability(website, save=True, log=True):
+    status_code = "-999"
+    try:
+        request = requests.get(website.url, timeout=30)
+        status_code = str(request.status_code)
+    except requests.exceptions.RequestException:
+        pass
+    if log:
+        print(website.name + " Status: " + status_code + "; ", end="\t")
+    if save:
+        up = 1 if status_code == "200" else 0
+        website.availability.append((int(time.time()), up))
+        monitor.create_json(website)
+    return status_code
+
+
+# Prints a big status update with the average
+# availability of the site over every data point
+def print_big_status():
+    print("")
+    print("===============================STATUS UPDATE===============================")
+    for status_site in websites:
+        avg_up = 0
+        for d in status_site.availability:
+            avg_up += d[1]
+        avg_up /= len(status_site.availability)
+        print("Average availability of " + status_site.name + ":\t" + str("%.3f" % avg_up) + " (Data size: " +
+              str(len(status_site.availability)) + ")")
+    print("===========================================================================")
+
+
+# MAIN BODY BEGINS HERE
+# First tries to load existing data if available
+# Then checks all the sites in websites for their availability until manually exited
+# Prints a big status every log_interval checks and small status updates in between
+sleep_time = 60  # How to long to wait between each round of checks
+small_sleep = 1  # How long to wait between each single page
+log_interval = 15  # How often does a big status get printed
+log_timer = 0  # Just counting
 for site in websites:
-    with open(site.name + ".json", "r") as file:
-        site.availability = monitor.read_json(file).availability
-    print("Starting monitoring of " + site.name)
-
+    load_existing_data(site)
 while True:
-    for site in websites:
-        request = requests.get(site.url)
-        up = 1 if request.status_code == 200 else 0
-        site.availability.append((int(time.time()), up))
-        status = str(up) if up == 1 else request.status_code
-        print("Checking: " + site.name + " Status: " + status + "; ", end="")
-        monitor.create_json(site)
-        time.sleep(2)
-    if log_timer % log_interval == 0:
-        print("")
-        print("===============================STATUS UPDATE===============================")
-        print("Monitoring for " + str(log_timer) + " minutes")
+    print("CHECKING: ")
+    if are_we_online():
         for site in websites:
-            avg_up = 0
-            for d in site.availability:
-                avg_up += d[1]
-            avg_up /= len(site.availability)
-            print("Average Uptime of " + site.name + ": " + str(avg_up))
-        print("===========================================================================")
-    log_timer += 1
+            check_availability(site, log=True)
+            time.sleep(small_sleep)
+        if log_timer % log_interval == 0:
+            print_big_status()
+        log_timer += 1
+    else:
+        print("NO INTERNET CONNECTION AVAILABLE", end="")
     timer = 0
     while timer < sleep_time:
         print(".", end="")
