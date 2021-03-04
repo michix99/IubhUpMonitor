1cc97c1a8062e9fabea75e04d83ef555adb5df85
Daniel Czeppel <danielczeppel@gmail.com>
2021-03-04 17:02:24 +0100

    cleaned up imports

--- monitor/monitor/main.py
+++ monitor/monitor/main.py
@@ -1,11 +1,4 @@
 import monitor
-import time
-
-from monitor import Website
-from monitor.Utils import plot_data, get_sec, get_past_utc, create_json, create_status_json, create_rest_json
-
-# Entry point to run the monitor (and optionally compress data)
-
-
+# Entry point to run the monitor
 m = monitor.Monitor()
 m.run_monitor()
