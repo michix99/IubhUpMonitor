4005f72efc74b433f0a7fd814e55094647596326
Michaela Andermann <michaela.andermann@gmx.de>
2021-04-07 22:57:09 +0200

    added initial web monitor project

---
+++ web-monitor/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  background: #2e2c2c;
+  // for firefox support
+  scrollbar-color: #554f4f #2e2c2c;
+  scrollbar-width: auto;
+}
+
+#app {
+  color: #ffffff;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+#nav {
+  padding: 30px 0 15px 0;
+
+  a {
+    font-weight: bold;
+    color: #ffffff;
+
+    &.router-link-exact-active {
+      color: #2086dabb;
+    }
+  }
+}
+
+// for chrome and safari
+::-webkit-scrollbar {
+  width: 12px;
+}
+
+::-webkit-scrollbar-track {
+  background: #2e2c2c;
+  // border: 2px solid #554f4f;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #554f4f;
+}
+
+</style>
---
+++ web-monitor/src/components/index.ts
@@ -0,0 +1,5 @@
+import Charts from './Charts.vue';
+import ErrorMessage from './ErrorMessage.vue';
+import Status from './Status.vue';
+
+export { Charts, ErrorMessage, Status };
