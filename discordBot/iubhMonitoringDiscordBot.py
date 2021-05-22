7f62dd74576d01efb72c657a0523f835fc05f37c
Daniel Czeppel <danielczeppel@gmail.com>
2021-05-22 13:36:55 +0200

    Fixed some typos, shortened some text

--- discordBot/iubhMonitoringDiscordBot.py
+++ discordBot/iubhMonitoringDiscordBot.py
@@ -2,16 +2,15 @@ import discord
 from discord.ext import commands
 import sys
 import os
-# appends the system path, to import the __init__ module from the monitor libary
-sys.path.append(os.path.join(os.path.split(os.getcwd())[0], 'monitor'))
+# appends the system path, to import the __init__ module from the monitor library
 from monitor.__init__ import *
+sys.path.append(os.path.join(os.path.split(os.getcwd())[0], 'monitor'))
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=intents)
 client.remove_command("help")
 
-
-# executes when bot connected successfully to the server
+# executes when bot connects successfully to the server
 @client.event
 async def on_ready():
     print(f'{client.user.name} successfully connect!\n')
@@ -20,59 +19,58 @@ async def on_ready():
         print(str(client.guilds[i]) + "\n")
         i += 1
 
-
-# generated message everytime when a new member joins the server
+# welcome message everytime a new member joins the server
 @client.event
 async def on_member_join(member):
     await member.create_dm()
-    await member.dm_channel.send(f"Welcome {member.name} to the unofficial IU server for computer scientiest and simulars. \n" 
-                                 f"At first, please introduce yourself at the channel 'vorstellungsrunde' and set your role"
-                                 f"based on the study you are doing at the IU. This will help other people to see at the first look"
-                                 f"what kind of study your are practising.\n\n"
-                                 f"To set your role please look at this list and type the correct command in our chat.\n"
-                                 f"WARNING: You can set your role only one time. If your set it wrong, wanna change it or your"
-                                 f"role isn't listed, then please contact an admin.\n"
-                                 f"Informatik - !informatik \nWirtschaftsinformatik - !wirtschaftsinformatik\n"
-                                 f"Medieninformatik - !medieninformatik \nComputer Science - !computer_science\n"
-                                 f"Robotik - !robotic \n Mit IT Bezug - !mITb \n\n"
-                                 f"For more instructions please fell free to ask the community or write !help in our chat"
-                                 f"so you can see what kind of action I'm supporting.\n"
+    await member.dm_channel.send(f"Welcome {member.name} to the unofficial IU server for IT courses. \n" 
+                                 f"Please introduce yourself at the channel '#vorstellungsrunde'"
+                                 f"You can use this bot to select a role to show everyone what you are studying.\n\n"
+                                 f"To set your role please type the correct command in our chat.\n"
+                                 f"If your set it wrong, want change it or your role is not listed, please contact"
+                                 f"one of the moderators.\n"
+                                 f"Informatik : !informatik \nWirtschaftsinformatik : !wirtschaftsinformatik\n"
+                                 f"Medieninformatik : !medieninformatik \nComputer Science : !computer_science\n"
+                                 f"Robotik : !robotic \n Mit IT Bezug : !mITb \n\n"
+                                 f"If you want to find out what else I can do, type !help in the chat!\n"
                                  f"Have fun and be nice to everyone. 😉")
 
 
-# help command to show the user all available functions and informations the bot have
+# help command to show the user all available functions and information the bot has
 @client.command()
 async def help(ctx):
     embed = discord.Embed(colour=discord.Colour.dark_red())
     embed.set_author(name="Help")
-    embed.add_field(name="Commands:", value="!status -> shows the availability of the most used websites as an IU-student")
+    embed.add_field(name="Commands:", value="!status -> shows the availability of IU websites")
     await ctx.author.send(embed=embed)
 
 
-# uses the read_json_status() method to get all the needed informations from the monitor and shows them in the chat
+# uses the read_json_status() method to get all the availability information from the monitor and shows them in the chat
 @client.command()
 async def status(ctx):
     answer_string = ""
-    status1 = read_json_data_bot()  # returns all necessary data for the user as a dictonary (function can be found in utils.py)
-    # creates an understandable message for the user with the informations from the dictonary
-    for i in range(len(status1)):
-        if status1.get(i)[3] == -1:
+    cur_status = read_json_data_bot()  # returns all necessary data for the user as a dictionary
+    # creates an understandable message for the user with the information from the dictionary
+    for i in range(len(cur_status)):
+        if cur_status.get(i)[3] == -1:
             last_off_time = "-"  # will be writen to the user if no "last offline time" is known
         else:
-            last_off_time = status1.get(i)[3]  # executes if a last offline time is known
-        answer_string += f"**{status1.get(i)[0]}**: \n" \
-                         f"\tStatus: {online_offline_unstable(status1.get(i)[1])} | Latency: {status1.get(i)[2]}ms | " \
+            last_off_time = cur_status.get(i)[3]  # executes if a last offline time is known
+        answer_string += f"**{cur_status.get(i)[0]}**: \n" \
+                         f"\tStatus: {online_offline_unstable(cur_status.get(i)[1])} " \
+                         f"| Latency: {cur_status.get(i)[2]}ms | " \
                          f"Last Offline-Time: {last_off_time}\n"
         i += 1
     # embeds the message for the user in a cooler look then a normal message
     embed = discord.Embed(colour=discord.Colour.dark_green())  # creates the embed and sets the border colour
-    embed.set_author(name="Status")  # "titel" of the message
-    embed.add_field(name=f"Online: {online_offline_unstable('GREEN')} Offline: {online_offline_unstable('RED')} Unstable: "
-                         f"{online_offline_unstable('YELLOW')}\n\n", value=answer_string)  # textfield with the inforamtion
+    embed.set_author(name="Status")  # "title" of the message
+    embed.add_field(name=f"Online: {online_offline_unstable('GREEN')} Offline: {online_offline_unstable('RED')} "
+                         f"Unstable: "
+                         f"{online_offline_unstable('YELLOW')}\n\n", value=answer_string)  # textfield with information
     await ctx.author.send(embed=embed)
 
 
-# currently stucked because of missing permission and i can't figure out what this missing permission is....
+# currently broken because of missing permission and i can't figure out what this missing permission is....
 @client.command()
 async def informatik(ctx):
     for i in range(len(ctx.author.guild.roles)):
@@ -87,5 +85,5 @@ async def informatik(ctx):
             print("role will be set")
 
 
-# runs the TOKEN of the choosen bot
+# runs the TOKEN of the chosen bot
 client.run('ODEwOTIxOTY0MTAwMjU1NzY0.YCqr7g.tGKPsONMJ5Yqj_PdiG0mgaWHyq0')
